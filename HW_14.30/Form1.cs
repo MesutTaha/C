b3fb7d637a395b34ecc29cf54eb49634a7dfcb0d
MesutTaha <71898824+MesutTaha@users.noreply.github.com>
2021-12-04 23:29:41 +0300

    next images

---
+++ HW_14.30/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW_14._30
+{
+    public partial class PictureBoxTest : Form
+    {
+        private int ImageNumber { get; set; } = -1;
+        public PictureBoxTest()
+        {
+            InitializeComponent();
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            ImageNumber = (ImageNumber + 1) % 3;
+
+            imagePictureBox.Image = (Image)(Properties.Resources.ResourceManager.GetObject($"image{ImageNumber}"));
+        }
+    }
+}
