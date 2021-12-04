97136b012e94a3e44f93af21677ad377365cf681
MesutTaha <71898824+MesutTaha@users.noreply.github.com>
2021-12-04 23:25:46 +0300

    button clicked-2

---
+++ HW_14.24/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace HW_14._24
+{
+    partial class GroupBoxPanelExample
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainGroupBox = new System.Windows.Forms.GroupBox();
+            this.hiButton = new System.Windows.Forms.Button();
+            this.byeButton = new System.Windows.Forms.Button();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.leftButton = new System.Windows.Forms.Button();
+            this.rightButton = new System.Windows.Forms.Button();
+            this.messageLabel = new System.Windows.Forms.Label();
+            this.mainGroupBox.SuspendLayout();
+            this.mainPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainGroupBox
+            // 
+            this.mainGroupBox.Controls.Add(this.byeButton);
+            this.mainGroupBox.Controls.Add(this.hiButton);
+            this.mainGroupBox.Location = new System.Drawing.Point(104, 41);
+            this.mainGroupBox.Name = "mainGroupBox";
+            this.mainGroupBox.Size = new System.Drawing.Size(200, 100);
+            this.mainGroupBox.TabIndex = 0;
+            this.mainGroupBox.TabStop = false;
+            this.mainGroupBox.Text = "Main GroupBox";
+            // 
+            // hiButton
+            // 
+            this.hiButton.Location = new System.Drawing.Point(6, 19);
+            this.hiButton.Name = "hiButton";
+            this.hiButton.Size = new System.Drawing.Size(75, 23);
+            this.hiButton.TabIndex = 0;
+            this.hiButton.Text = "Hi";
+            this.hiButton.UseVisualStyleBackColor = true;
+            this.hiButton.Click += new System.EventHandler(this.hiButton_Click);
+            // 
+            // byeButton
+            // 
+            this.byeButton.Location = new System.Drawing.Point(119, 19);
+            this.byeButton.Name = "byeButton";
+            this.byeButton.Size = new System.Drawing.Size(75, 23);
+            this.byeButton.TabIndex = 1;
+            this.byeButton.Text = "Bye";
+            this.byeButton.UseVisualStyleBackColor = true;
+            this.byeButton.Click += new System.EventHandler(this.byeButton_Click);
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.AutoScroll = true;
+            this.mainPanel.Controls.Add(this.rightButton);
+            this.mainPanel.Controls.Add(this.leftButton);
+            this.mainPanel.Location = new System.Drawing.Point(104, 147);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(200, 83);
+            this.mainPanel.TabIndex = 1;
+            // 
+            // leftButton
+            // 
+            this.leftButton.Location = new System.Drawing.Point(3, 3);
+            this.leftButton.Name = "leftButton";
+            this.leftButton.Size = new System.Drawing.Size(75, 23);
+            this.leftButton.TabIndex = 1;
+            this.leftButton.Text = "Far Left";
+            this.leftButton.UseVisualStyleBackColor = true;
+            this.leftButton.Click += new System.EventHandler(this.leftButton_Click);
+            // 
+            // rightButton
+            // 
+            this.rightButton.Location = new System.Drawing.Point(187, 3);
+            this.rightButton.Name = "rightButton";
+            this.rightButton.Size = new System.Drawing.Size(75, 23);
+            this.rightButton.TabIndex = 2;
+            this.rightButton.Text = "Far Right";
+            this.rightButton.UseVisualStyleBackColor = true;
+            this.rightButton.Click += new System.EventHandler(this.rightButton_Click);
+            // 
+            // messageLabel
+            // 
+            this.messageLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.messageLabel.Location = new System.Drawing.Point(122, 272);
+            this.messageLabel.Name = "messageLabel";
+            this.messageLabel.Size = new System.Drawing.Size(164, 23);
+            this.messageLabel.TabIndex = 2;
+            // 
+            // GroupBoxPanelExample
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 343);
+            this.Controls.Add(this.messageLabel);
+            this.Controls.Add(this.mainPanel);
+            this.Controls.Add(this.mainGroupBox);
+            this.Name = "GroupBoxPanelExample";
+            this.Text = "GroupBox and Panel";
+            this.mainGroupBox.ResumeLayout(false);
+            this.mainPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox mainGroupBox;
+        private System.Windows.Forms.Button byeButton;
+        private System.Windows.Forms.Button hiButton;
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.Button rightButton;
+        private System.Windows.Forms.Button leftButton;
+        private System.Windows.Forms.Label messageLabel;
+    }
+}
+
---
+++ HW_14.24/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW_14._24
+{
+    public partial class GroupBoxPanelExample : Form
+    {
+        public GroupBoxPanelExample()
+        {
+            InitializeComponent();
+        }
+
+        private void hiButton_Click(object sender, EventArgs e)
+        {
+            messageLabel.Text = "Hi pressed";
+        }
+
+        private void byeButton_Click(object sender, EventArgs e)
+        {
+            messageLabel.Text = "Bye pressed";
+        }
+
+        private void leftButton_Click(object sender, EventArgs e)
+        {
+            messageLabel.Text = "Far Left Pressed";
+        }
+
+        private void rightButton_Click(object sender, EventArgs e)
+        {
+            messageLabel.Text = "Far Right Pressed ";
+        }
+    }
+}
